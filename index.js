14d4402ffc3f041527f592a0e58f82f7764bc591
steve prefect <steve prefect>
2017-04-03 12:10:55 -0400

    initial commit

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ var fs = require('fs');
 var kdt = require('kdt');
 var zips = [];
 try {
-	// zip code data is sorted by ascending latitude
-	// From the Puerto Rico to Alaska
-	var data = fs.readFileSync(__dirname + '/zip_codes_sorted.json', 'ascii');
+    // zip code data is sorted by ascending latitude
+    // From the Puerto Rico to Alaska
+    var data = fs.readFileSync(__dirname + '/Craigslistdomains.json', 'ascii');
 
-	//zips is a global defined up top
-	zips = JSON.parse(data);
-	var distance = function(a, b){
-		return Math.pow(a.latitude - b.latitude, 2) +  Math.pow(a.longitude - b.longitude, 2);
-	};
-	var tree = kdt.createKdTree(zips, distance, ['latitude', 'longitude']);
+    //zips is a global defined up top
+    zips = JSON.parse(data);
+    var distance = function(a, b) {
+        return Math.pow(a.latitude - b.latitude, 2) + Math.pow(a.longitude - b.longitude, 2);
+    };
+    var tree = kdt.createKdTree(zips, distance, ['latitude', 'longitude']);
 } catch (err) {
-	console.error("There was an error opening the zip code file:");
-	console.log(err);
+    console.error("There was an error opening the zip code file:");
+    console.log(err);
 }
 
-exports.gps2zip = function(latitude, longitude){
-	// we've already constructed the kd-tree. Just find the nearest location
-	var results =  tree.nearest({'latitude': latitude, 'longitude': longitude}, 1);
-	var firstResult = results[0];
-	var zipCode = firstResult[0];
-	return zipCode;
+exports.gps2cl = function(latitude, longitude) {
+    // we've already constructed the kd-tree. Just find the nearest location
+    var results = tree.nearest({ 'latitude': latitude, 'longitude': longitude }, 1);
+    var firstResult = results[0];
+    var zipCode = firstResult[0];
+    return zipCode;
 };
--- readme.md
+++ readme.md
@@ -1,36 +1,27 @@
 
-# Gps2zip
+# Gps2cl
 
-Convert GPS latitude longitude pairs to a US zip code.
+Convert GPS latitude longitude pairs to the closest craigslist domain.
 
 # Quick Start
 
-  npm install gps2zip
+  npm install gps2cl
 
 # Usage
 
-  var gps = require('gps2zip');
+  var gps = require('gps2cl');
   var latitude = 30.2669;
   var longitude = 97.7428;
-  gps.gps2zip(latitude, longitude);
-  // returns 78701
+  gps.gps2cl(latitude, longitude);
+  // returns "austin"
 
-# Unit tests
-
-  node test/basic.js
 
 # Accuracy
 The output is approximate. The source data is just a list of coordinates, not actual boundaries. The algorithm just finds the minimum euclidean distance between the point in question and the known points. No error checking for points outside the United States.
 
 
-# Related
-
-You can get full street addresses using the Google Maps Reverse Geocoding API:
-http://code.google.com/apis/maps/documentation/javascript/geocoding.html#ReverseGeocoding
-
 # Credit
 
-Zip codes from The Zip Code Database Project
-http://sourceforge.net/projects/zips/
+I found the list of Craigslist subdomains on the internet somewhere, but I can't find them now.
 
 
